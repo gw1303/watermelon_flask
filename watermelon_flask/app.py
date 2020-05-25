7d75b8ffa27e2a6107ee66d1396a4f2acb5e7715
Redix8 <40425965+Redix8@users.noreply.github.com>
2020-05-25 21:37:24 +0900

    strip all receives, find song by id.

--- watermelon_flask/app.py
+++ watermelon_flask/app.py
@@ -7,17 +7,14 @@ import pickle
 
 app = Flask(__name__)
 
-
 modelPath = '/home/ubuntu/watermelon/song2vec/'
-dataPath  = '/home/ubuntu/watermelon/data/'
+dataPath = '/home/ubuntu/watermelon/data/'
 
-
-if len(sys.argv)>1 and sys.argv[1] == 'dev':
+if len(sys.argv) > 1 and sys.argv[1] == 'dev':
     modelPath = 'C:/melon/'
-    dataPath  = 'C:/melon/'
+    dataPath = 'C:/melon/'
 
 model = Song2Vec(path=modelPath)
-
 
 songDf = pd.read_json(dataPath + 'song_meta.json')
 
@@ -25,14 +22,14 @@ songDf = pd.read_json(dataPath + 'song_meta.json')
 def loadUser(userId):
     try:
         with open(f'users/{userId}.bin', 'rb') as f:
-            user = pickle.load(f) # 
+            user = pickle.load(f)  #
             return user
     except FileNotFoundError:
-        user = {'userId': userId, 'myPlaylist':[]}        
+        user = {'userId': userId, 'myPlaylist': []}
         return user
     except:
         return False
-    
+
 
 def saveUser(userId, user):
     try:
@@ -42,24 +39,25 @@ def saveUser(userId, user):
     except:
         return False
 
+
 def makeQuickReply(outputText, labels, action, messages=None, blockId=None, extra=None):
     res = {
-            'version': "2.0",
-            'template': {
-                'outputs': [{
-                    'simpleText': {
-                        'text': f'{outputText}'
-                    }
-                }],
-                'quickReplies': []                            
-            }
+        'version': "2.0",
+        'template': {
+            'outputs': [{
+                'simpleText': {
+                    'text': f'{outputText}'
+                }
+            }],
+            'quickReplies': []
         }
-    
+    }
+
     for i in range(len(labels)):
         item = {
-            'label'  : labels[i],
-            'action' : action,
-            'messageText' : messages[i] if messages else labels[i]
+            'label': labels[i],
+            'action': action,
+            'messageText': messages[i] if messages else labels[i]
         }
         if action == 'block':
             item['blockId'] = blockId
@@ -68,6 +66,13 @@ def makeQuickReply(outputText, labels, action, messages=None, blockId=None, extr
         res['template']['quickReplies'].append(item)
 
     return res
+
+
+def findSongById(sid, df=songDf):
+    song = df.iloc[sid].song_name
+    artist = df.iloc[sid].artist_name_basket
+    return f'{song}-{artist}'
+
 
 # genreDf = pd.DataFrame(pd.read_json(dataPath + 'genre_gn_all.json', encoding='utf-8', typ='series'), columns=['genre'])
 # genreDfIndex = list(genreDf.index)
@@ -78,13 +83,12 @@ def makeQuickReply(outputText, labels, action, messages=None, blockId=None, extr
 # tag = []
 # for i in playlistDf.tags :
 #     tag += i
-    
+
 # tagUnique = list(set(tag))
 
 
 @app.route("/")
 def hello():
-    
     print('print')
 
     return 'hello'
@@ -97,7 +101,6 @@ def block():
     return_str = req['userRequest']
     return_str = str(return_str)
 
-    
     res = {
         'version': "2.0",
         'template': {
@@ -112,8 +115,6 @@ def block():
     return jsonify(res)
 
 
-
-
 @app.route("/message", methods=['POST'])
 def message():
     req = request.get_json()
@@ -122,7 +123,6 @@ def message():
 
     # 필요한 변수들 
     global songDf
-
 
     if return_str == '시작':
         res = {
@@ -139,12 +139,12 @@ def message():
                     'messageText': '음악추가',
 
                 },
-                {
-                    'label': '음악추천',
-                    'action': 'message',
-                    'messageText': '음악추천',
+                    {
+                        'label': '음악추천',
+                        'action': 'message',
+                        'messageText': '음악추천',
 
-                }]
+                    }]
             }
         }
 
@@ -164,19 +164,18 @@ def message():
 
         return jsonify(res)
 
-    elif return_str.find('-') != -1 :
+    elif return_str.find('-') != -1:
 
         artist, song = return_str.split('-')
 
         # 입력받은 가수와 제목으로 df 구성
         findArtistDf = songDf[songDf.artist_name_basket.str.contains(artist.strip())].sort_values(by='song_name')
-        
-        if len(findArtistDf.song_name.str.replace(' ', '').str.contains(song.strip())) > 0 :
+
+        if len(findArtistDf.song_name.str.replace(' ', '').str.contains(song.strip())) > 0:
             findSongDf = findArtistDf[findArtistDf.song_name.str.replace(' ', '').str.contains(song.strip())]
-        else :
+        else:
             findSongDf = findArtistDf
 
-            
         # 길이순 재 정렬
         idx = findSongDf['song_name'].str.len().sort_values().index
         findSongDf = findSongDf.reindex(idx)
@@ -184,58 +183,58 @@ def message():
         txt = '몇번째 음악을 추가하시겠습니까?\n\n'
         quickReplies = []
 
-        for i in range(len(findSongDf)) :  
+        for i in range(len(findSongDf)):
 
             song = findSongDf.iloc[i].song_name
             artist = findSongDf.iloc[i].artist_name_basket
             album = findSongDf.iloc[i].album_name
             songId = findSongDf.iloc[i].name
 
-            txt += '{}번 {} - {} / {}\n\n'.format((i+1), artist, song, album)  # song_name 출력 
-             
-            quickReplies. append({
-                    'label': str(i+1),
-                    'action': 'block',
-                    'messageText': f'{i+1}번 노래가 추가되었습니다.',
-                    'blockId' : '5ecb168c031ba400011698b3',
-                    'extra' : {'songId':str(songId)},
-                    })
+            txt += '{}번 {} - {} / {}\n\n'.format((i + 1), artist, song, album)  # song_name 출력
 
-            if i == 9 :
+            quickReplies.append({
+                'label': str(i + 1),
+                'action': 'block',
+                'messageText': f'{i + 1}번 노래가 추가되었습니다.',
+                'blockId': '5ecb168c031ba400011698b3',
+                'extra': {'songId': str(songId)},
+            })
+
+            if i == 9:
                 break
-
-        res = {
-                'version': "2.0",
-                'template': {
-                    'outputs': [{
-                        'simpleText': {
-                            'text': txt  # song_name 출력 
-                        }
-                    }],
-                    'quickReplies': quickReplies  # 몇번째 노래 ? 
-                    }
-                }
-
-        return jsonify(res)
-
-
-
-
-
-    elif return_str == '음악추천':
-        req = request.get_json()
-        userId = req['userRequest']['user']['id']
-        userId = str(userId)
-        user = loadUser(userId)
-
-        pred = model.getRecommendation(songs=user['myPlaylist'])
 
         res = {
             'version': "2.0",
             'template': {
                 'outputs': [{
                     'simpleText': {
-                        'text': str(pred)
+                        'text': txt  # song_name 출력
+                    }
+                }],
+                'quickReplies': quickReplies  # 몇번째 노래 ?
+            }
+        }
+
+        return jsonify(res)
+
+
+    elif return_str == '음악추천':
+        req = request.get_json()
+        userId = req['userRequest']['user']['id']
+        userId = str(userId).strip()
+        user = loadUser(userId)
+
+        pred = model.getRecommendation(songs=user['myPlaylist'])
+        songs = ''
+        for sid, prob in pred:
+            songs += findSongById(sid) + '\n'
+
+        res = {
+            'version': "2.0",
+            'template': {
+                'outputs': [{
+                    'simpleText': {
+                        'text': songs
                     }
                 }]
             }
@@ -243,7 +242,7 @@ def message():
 
         return jsonify(res)
 
-    else : 
+    else:
         res = {
             'version': "2.0",
             'template': {
@@ -256,18 +255,19 @@ def message():
         }
 
         return jsonify(res)
+
+
 @app.route("/addMusic", methods=['POST'])
-def addMusic() :
+def addMusic():
     req = request.get_json()
     songId = req['action']['clientExtra']['songId']
     songId = str(songId).strip()
 
     userId = req['userRequest']['user']['id']
-    userId = str(userId)
+    userId = str(userId).strip()
 
     user = loadUser(userId)
     user['myPlaylist'].append(songId)
-    
 
     res = {
         'version': "2.0",
@@ -283,10 +283,9 @@ def addMusic() :
     return jsonify(res)
 
 
-
 # 메인 함수
 if __name__ == '__main__':
-    if len(sys.argv)>1 and sys.argv[1] == 'dev':
+    if len(sys.argv) > 1 and sys.argv[1] == 'dev':
         app.run(debug=True)
     else:
         app.run(host='0.0.0.0', port=5000, threaded=True)
