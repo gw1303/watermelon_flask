d28cb2ee1fe63a75dcb5d861ea19c5d7fa5e776f
redix8 <kukko@paran.com>
2020-05-25 10:02:07 +0900

    error fix

--- watermelon_flask/app.py
+++ watermelon_flask/app.py
@@ -18,13 +18,14 @@ playlistDf = pd.read_json('/home/ubuntu/watermelon/data/train.json')
 
 # tag
 tag = []
-for i in playlistDf.tags :
+for i in playlistDf.tags:
     tag += i
-    
+
 tagUnique = list(set(tag))
 
+
 @app.route("/selectMode")
-def selectMode() :
+def selectMode():
     req = request.get_json()
     return_str = req['userRequest']['utterance']
     return_str = str(return_str).strip()
@@ -43,7 +44,7 @@ def selectMode() :
                     'action': 'message',
                     'messageText': '처음으로'
                 },
-                {
+                    {
                     'label': '추   천',
                     'action': 'message',
                     'messageText': '처음으로'
@@ -53,18 +54,16 @@ def selectMode() :
 
         return jsonify(res)
 
+def getSongId():
 
-
-def getSongId() :
-    
     req = request.get_json()
     return_str = req['userRequest']['utterance']
     userSongName = str(return_str).strip()
     userSongName = userSongName.lower()
 
-    try :
+    try:
         artist, song = userSongName.split('-')
-    except ValueError :
+    except ValueError:
 
         res = {
             'version': "2.0",
@@ -85,66 +84,65 @@ def getSongId() :
         return jsonify(res)
 
     # 입력받은 가수와 제목으로 df 구성
-    findArtistDf = songDf[songDf.artist_name_basket.str.contains(artist.strip())].sort_values(by='song_name')
-    
-    if len(findArtistDf.song_name.str.replace(' ', '').str.contains(song.strip())) > 0 :
-        findSongDf = findArtistDf[findArtistDf.song_name.str.replace(' ', '').str.contains(song.strip())]
-    else :
+    findArtistDf = songDf[songDf.artist_name_basket.str.contains(
+        artist.strip())].sort_values(by='song_name')
+
+    if len(findArtistDf.song_name.str.replace(' ', '').str.contains(song.strip())) > 0:
+        findSongDf = findArtistDf[findArtistDf.song_name.str.replace(
+            ' ', '').str.contains(song.strip())]
+    else:
         findSongDf = findArtistDf
-
-
-    
     userSelect = 999999999
-    
+
     # 검색된 노래가 2개 이상일 경우 선택받는다
-    if len(findSongDf) > 1 :
-        
+    if len(findSongDf) > 1:
+
         # 길이순 재 정렬
         idx = findSongDf['song_name'].str.len().sort_values().index
         findSongDf = findSongDf.reindex(idx)
-            
+
         print('무슨 노래입니까 ?', end='\n\n')
 
         saaList = []
 
-        for i in range(5) :  # len(findSongDf)
+        for i in range(5):  # len(findSongDf)
 
             song = findSongDf.iloc[i].song_name
             artist = findSongDf.iloc[i].artist_name_basket
             album = findSongDf.iloc[i].album_name
-            saaList.append([aong, artist, album])
-        
+            saaList.append([song, artist, album])
+
         res = {
             'version': "2.0",
             'template': {
                 'outputs': [{
                     'listCard': {
-                        'header' : {
-                            'title' : '무슨 노래입니까?'
+                        'header': {
+                            'title': '무슨 노래입니까?'
                         },
-                        'items' : [
+                        'items': [
                             {
-                            'title' : saaList[0][1]+saaList[0][0]
-                            'description' : saaList[0][2]
+                                'title': saaList[0][1]+saaList[0][0],
+                                'description': saaList[0][2]
                             },
                             {
-                            'title' : saaList[1][1]+saaList[1][0]
-                            'description' : saaList[1][2]
+                                'title': saaList[1][1]+saaList[1][0],
+                                'description': saaList[1][2]
                             },
                             {
-                            'title' : saaList[2][1]+saaList[2][0]
-                            'description' : saaList[2][2]
-                            },3
-                            {4
-                            'title' : saaList[3][1]+saaList[3][0]
-                            'description' : saaList[3][2]
-                            },
+                                'title': saaList[2][1]+saaList[2][0],
+                                'description': saaList[2][2]
+                            }, 
                             {
-                            'title' : saaList[4][1]+saaList[4][0]
-                            'description' : saaList[4][2]
+                             'title': saaList[3][1]+saaList[3][0],
+                             'description': saaList[3][2]
+                             },
+                            {
+                                'title': saaList[4][1]+saaList[4][0],
+                                'description': saaList[4][2]
                             },
                         ]
-    
+
                     }
                 }],
                 'quickReplies': [{
@@ -155,31 +153,31 @@ def getSongId() :
             }
         }
 
-        while userSelect >= len(findSongDf) :
+        while userSelect >= len(findSongDf):
             userSelect = int(input('번호를 입력하시오.')) - 1
-        
-        
-        print("\n%s 의 ['%s'] 곡이 추가되었습니다." %(findSongDf.iloc[userSelect].artist_name_basket,
-                                          findSongDf.iloc[userSelect].song_name))
-        
+
+        print("\n%s 의 ['%s'] 곡이 추가되었습니다." % (findSongDf.iloc[userSelect].artist_name_basket,
+                                             findSongDf.iloc[userSelect].song_name))
+
         return findSongDf.iloc[userSelect].id
-   # 검색된 노래가 하나일 경우 
-    else :
-        print("\n%s 의 ['%s'] 곡이 추가되었습니다." %(findSongDf.artist_name_basket.tolist()[0], findSongDf.song_name.tolist()[0]))
-        
+   # 검색된 노래가 하나일 경우
+    else:
+        print("\n%s 의 ['%s'] 곡이 추가되었습니다." % (
+            findSongDf.artist_name_basket.tolist()[0], findSongDf.song_name.tolist()[0]))
+
         return findSongDf.id.tolist()[0]
 
 
 @app.route("/")
 def hello():
-    
+
     global a
     global songDf
 
     a += 1
 
-
     return str(a) + songDf['song_name'][a]
+
 
 @app.route("/message", methods=['POST'])
 def message():
